02edcad66ac6269b3af044844af93d3c7d2ec3ce
CastellierGwenole <castellier.gwenole@gmail.com>
2018-07-23 15:53:04 +0200

    test

--- javascript.js
+++ javascript.js
@@ -1,32 +1,31 @@
-navigator.getUserMedia(constraints, successCallback, errorCallback);
-
+document.getElementById("x").innerHTML = "x :";
+document.getElementById("y").innerHTML = "y :";
+document.getElementById("z").innerHTML = "z :";
 function test() {
-    document.addEventListener("keydown", keyDownHandler, false);
-    document.addEventListener("keyup", keyUpHandler, false);
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
 }
 
 setInterval(test, 10);
 
-function keyDownHandler(e){
-document.getElementById("test").innerHTML = "jean";
+function keyDownHandler(e) {
+  document.getElementById("test").innerHTML = "jean";
 }
 
-function keyUpHandler(e){
-    
+function keyUpHandler(e) {
+
 }
 
-if(window.DeviceMotionEvent) {
-    window.addEventListener("devicemotion", process, false);
-  } else {
-    // Le navigateur ne supporte pas l'événement devicemotion
-  }
+if (window.DeviceMotionEvent) {
+  window.addEventListener("devicemotion", process, false);
+} else {}
 
-  function process(event) {
-    var x = Math.round(100*event.accelerationIncludingGravity.x)/100;
-    var y = Math.round(100*event.accelerationIncludingGravity.y)/100;
-    var z = Math.round(100*event.accelerationIncludingGravity.z)/100;
-document.getElementById("x").innerHTML = "x :"+ x;
-document.getElementById("y").innerHTML = "y :"+ y;
-document.getElementById("z").innerHTML = "z :"+ z;
-    
-  }
+function process(event) {
+  var x = Math.round(100 * event.accelerationIncludingGravity.x) / 100;
+  var y = Math.round(100 * event.accelerationIncludingGravity.y) / 100;
+  var z = Math.round(100 * event.accelerationIncludingGravity.z) / 100;
+  document.getElementById("x").innerHTML = "x :" + x;
+  document.getElementById("y").innerHTML = "y :" + y;
+  document.getElementById("z").innerHTML = "z :" + z;
+
+}

5f95f57eaf7e8c33ea323cd9af96ff79f58f7e57
CastellierGwenole <castellier.gwenole@gmail.com>
2018-07-23 16:19:48 +0200

    test

--- javascript.js
+++ javascript.js
@@ -1,13 +1,17 @@
 var video = document.querySelector("#videoElement");
- 
-if (navigator.mediaDevices.getUserMedia) {       
-    navigator.mediaDevices.getUserMedia({video: true})
-  .then(function(stream) {
-    video.srcObject = stream;
-  })
-  .catch(function(err0r) {
-    console.log("Something went wrong!");
-  });
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: "environment"
+      }
+    })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
 }
 
 function test() {
